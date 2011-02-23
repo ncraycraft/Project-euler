9830e97d9bf1e31e5ae9714dec58a90a5421716f
Nick Craycraft <gabbalis@gmail.com>
2011-02-23 00:37:28 -0500

    these .metdatas are everywhere!

--- euler/euler1.js
+++ euler/euler1.js
@@ -1,3 +1,4 @@
+
 var d=new Date();
 var theDay=d.getDay();
 var r = confirm("Do You wish to know the day?");
@@ -28,19 +29,23 @@ if (r == true);
 		break;
 	default:
 		document.write("I'm looking forward to this weekend!");
-}
-}
-var i=0;
-for (i=0;i<=5;i++)
-{
-document.write("The number is " + i);
-document.write("<br />");
-}
-var i=0;
-
-while (i<=5)
-  {
-  document.write("The number is " + i);
-  document.write("<br />");
-  i++;
-  }
+	}
+	}
+elce
+	{
+		var i=0;
+		for (i=0;i<=5;i++)
+		{
+		document.write("The number is " + i);
+		document.write("<br />");
+		}
+		var i=0;
+	
+		while (i<=5)
+		{
+		document.write("The number is " + i);
+		document.write("<br />");
+		i++;
+		}
+	}	
+	
