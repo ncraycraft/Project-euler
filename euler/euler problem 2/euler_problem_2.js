3eac3e5e934f1b7b002a7453b8cdf5394ea74406
Nick Craycraft <gabbalis@gmail.com>
2011-02-24 00:50:00 -0500

    making it look good

--- euler/euler problem 2/euler_problem_2.js
+++ euler/euler problem 2/euler_problem_2.js
@@ -1,14 +1,13 @@
-var highnum=1;
-var lownum=1;
-var sum=0;
+var highnum = 1;
+var lownum = 1;
+var sum = 0;
 
-while (highnum<=4000000) {
-	highnum=highnum+lownum;
-	lownum=highnum-lownum;
-	
-	if (highnum%2==0) {
-		sum=highnum+sum;
-	}
+while (highnum <= 4000000) {
+    highnum = highnum + lownum;
+    lownum = highnum - lownum;
+    if (highnum % 2 == 0) {
+        sum = highnum + sum;
+    }
 }
 console.log(sum);
 
